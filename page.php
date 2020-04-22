1c43091f6ad911bea6fa9f61c9346947a8872f4e
Sam <super8989@gmail.com>
2020-04-22 15:20:03 -0400

    Menu of child page links

--- page.php
+++ page.php
@@ -23,10 +23,11 @@
 <div class="container container--narrow page-section">
 
   <?php
-    $theParent = wp_get_post_parent_id(get_the_ID());
+    $theParent = wp_get_post_parent_id(get_the_ID()); // if child = parent id; if parent = 0
 
       if ($theParent) { ?>
 
+  <!-- Back to.. Menu -->
   <div class="metabox metabox--position-up metabox--with-home-link">
     <p>
       <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>">
@@ -38,14 +39,39 @@
   <?php  } ?>
 
 
+  <?php
 
-  <!-- <div class="page-links">
-    <h2 class="page-links__title"><a href="#">About Us</a></h2>
+  $testArray = get_pages(array(
+    'child_of' => get_the_ID()
+  ));
+  
+      if ($theParent or $testArray) { ?>
+
+  <!-- The side menu -->
+  <div class="page-links">
+    <h2 class="page-links__title">
+      <a href="<?php echo get_permalink($theParent); ?>">
+        <?php echo get_the_title($theParent); ?>
+      </a>
+    </h2>
     <ul class="min-list">
-      <li class="current_page_item"><a href="#">Our History</a></li>
-      <li><a href="#">Our Goals</a></li>
+      <?php
+        if ($theParent) {
+            $findChildrenOf = $theParent;
+        } else {
+            $findChildrenOf = get_the_ID();
+        }
+
+          wp_list_pages(array(
+          'title_li' => null,
+          'child_of' => $findChildrenOf,
+          'sort_column' => 'menu_order'
+        )); ?>
     </ul>
-  </div> -->
+  </div>
+
+  <?php
+      } ?>
 
   <div class="generic-content">
     <?php the_content(); ?>
