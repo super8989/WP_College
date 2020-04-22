81aad74b456a69a6eac7f1344f6d1e4323fe77d4
Sam <super8989@gmail.com>
2020-04-22 11:03:01 -0400

    convert static HTML template into WordPress

--- functions.php
+++ functions.php
@@ -2,9 +2,15 @@
 
 function university_files()
 {
-    // load CSS or JS files
+    //load JS script
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), null, '1.0', true);
+    // wp function('file name', 'location', 'does it have dependencies?', 'version', 'load before closing body tag?')
+
+    // load multiple CSS
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_stylesheet_uri());
     // wp function('main stylesheet', 'location of file' (or wp function))
 }
 
-add_action('wp_enqueue_scripts', 'university_files'); // wp function('load a CSS or JS file', 'function')
+add_action('wp_enqueue_scripts', 'university_files'); // wp function('load a CSS or JS file', 'function name')
