4bfa432c9b70268b15e3b3fa6f7010d868654c43
Sam <super8989@gmail.com>
2020-04-22 11:21:13 -0400

    Avoiding caching for CSS and JS during development

--- functions.php
+++ functions.php
@@ -3,14 +3,14 @@
 function university_files()
 {
     //load JS script
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), null, '1.0', true);
-    // wp function('file name', 'location', 'does it have dependencies?', 'version', 'load before closing body tag?')
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), null, microtime(), true);
+    // wp function('file name', 'location', 'does it have dependencies?', 'version' (during dev, microtime function to reload new scripts every refresh = avoids caching, 'load before closing body tag?')
 
     // load multiple CSS
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
-    // wp function('main stylesheet', 'location of file' (or wp function))
+    wp_enqueue_style('university_main_styles', get_stylesheet_uri(), null, microtime());
+    // wp function('main stylesheet', 'location of file' (or wp function), dependecies, version)
 }
 
 add_action('wp_enqueue_scripts', 'university_files'); // wp function('load a CSS or JS file', 'function name')
