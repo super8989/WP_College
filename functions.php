c285b80f0d5ea844733843d0fa3c690444086fb0
Sam <super8989@gmail.com>
2020-04-23 21:40:40 -0400

    Creating a relationship between content

--- functions.php
+++ functions.php
@@ -51,6 +51,12 @@ add_action('init', 'university_post_types');  // ('hook', 'function name') */
 
 function university_adjust_queries($query)
 {
+    if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('posts_per_page', -1);
+    }
+
     if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
         $today = date('Ymd');
 
