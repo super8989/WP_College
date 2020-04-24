c09f40eb8425e4deb3a8aee402bad3299b481922
Sam <super8989@gmail.com>
2020-04-23 21:00:49 -0400

    Past events page - custom query pagination

--- page-past-events.php
+++ page-past-events.php
@@ -1,6 +1,4 @@
-<!-- Event archive page: Event main page -->
-
-<!-- Category/Author/Date archive page of Blog -->
+<!-- Page for Past Events  -->
 
 <?php
   get_header(); ?>
@@ -10,17 +8,36 @@
     style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>);">
   </div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"> All Events </h1>
+    <h1 class="page-banner__title"> Past Events </h1>
     <div class="page-banner__intro">
-      <p>See what is going on in our world</p>
+      <p>A recap of our past events</p>
     </div>
   </div>
 </div>
 
 <div class="container container--narrow page-section">
   <?php
-    while (have_posts()) {
-        the_post(); ?>
+
+    $today = date('Ymd');
+        $pastEvents = new WP_Query(array(
+          'paged' => get_query_var('paged', 1),
+          'post_type' => 'event',
+          'meta_key' => 'event_date',
+          'orderby' => 'meta_value_num',
+          'order' => 'ASC',
+          'meta_query' => array(
+            array(
+              'key' => 'event_date',
+              'compare' => '<',
+              'value' => $today,
+              'type' => 'numeric'
+            )
+          )
+        ));
+
+
+    while ($pastEvents->have_posts()) {
+        $pastEvents->the_post(); ?>
 
   <div class="event-summary">
     <a class="event-summary__date t-center" href="#">
@@ -48,15 +65,10 @@
   <?php
     }
 
-    echo paginate_links();
+    echo paginate_links(array(
+      'total' => $pastEvents->max_num_pages
+    ));
   ?>
-
-  <hr class="section-break">
-
-  <p>Looking for a recap of past events?
-    <a href='<?php echo site_url('/past-events') ?>'>Check out our past events
-      archive.</a>
-  </p>
 
 </div>
 
