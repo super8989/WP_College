70285c17e8e4b207a4d6cb14ea38b895cd3571b4
Sam <super8989@gmail.com>
2020-04-23 20:38:35 -0400

    Manipulating default URL based queries

--- functions.php
+++ functions.php
@@ -47,3 +47,25 @@ add_action('after_setup_theme', 'university_features');
 
 // create new post type
 add_action('init', 'university_post_types');  // ('hook', 'function name') */
+
+
+function university_adjust_queries($query)
+{
+    if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
+        $today = date('Ymd');
+
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            array(
+              'key' => 'event_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric'
+            )
+          ));
+    }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
