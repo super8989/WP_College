d9671f634fc41501a47f8c07992c1c7c41a98593
Sam <super8989@gmail.com>
2020-04-26 15:10:04 -0400

    Using pageBanner funciton

--- single-professor.php
+++ single-professor.php
@@ -3,14 +3,12 @@
   get_header();
 
   while (have_posts()) {
-      the_post();
-      pageBanner();
+    the_post();
+    pageBanner();
 ?>
-    
 
     <div class="container container--narrow page-section">
           
-
       <div class="generic-content">
         <div class="row group">
 
@@ -29,24 +27,22 @@
 
         $relatedPrograms = get_field('related_programs');
 
-      if ($relatedPrograms) {
+        if ($relatedPrograms) {
           echo '<hr class="section-break">';
           echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
           echo '<ul class="link-list min-list">';
           
-          foreach ($relatedPrograms as $program) { ?>
+          foreach ($relatedPrograms as $program) { 
+      ?>
+
             <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+
           <?php }
           echo '</ul>';
-      } ?>
+        } ?>
 
     </div>
-    
-
-    
-  <?php
-  }
-
-  get_footer();
-
-?>
+ 
+  <?php }
+    get_footer();
+  ?>
