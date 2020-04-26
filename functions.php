95494e6a106216ca1e4cf9860dc7f6b6af80964b
Sam <super8989@gmail.com>
2020-04-26 12:25:03 -0400

    Featured image sizes and cropping

--- functions.php
+++ functions.php
@@ -2,55 +2,23 @@
 
 function university_files()
 {
-    //load JS script
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), null, microtime(), true);
-    // wp function('file name', 'location', 'does it have dependencies?', 'version' (during dev, microtime function to reload new scripts every refresh = avoids caching, 'load before closing body tag?')
-
-    // load multiple CSS
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), null, '1.0', true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri(), null, microtime());
-    // wp function('main stylesheet', 'location of file' (or wp function), dependecies, version)
+    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 }
 
-add_action('wp_enqueue_scripts', 'university_files'); // wp function('load a CSS or JS file', 'function name')
-
+add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features()
 {
-    // dynamic navigation menu
-    // register_nav_menu('headerMenuLocation', 'Header Menu Location'); // adds menu option to the theme -> ('menu location', 'name to show in WP admin' )
-    // register_nav_menu('footerLocationOne', 'Footer Location One');
-    // register_nav_menu('footerLocationTwo', 'Footer Location Two');
-    add_theme_support('title-tag'); // adds document title tag to HTML -> browser tab title
+    add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_image_size('professorLandscape', 400, 260, true);
     add_image_size('professorPortrait', 480, 650, true);
 }
 
 add_action('after_setup_theme', 'university_features');
-
-
-// Creating a new post type, Event --> moved to mu-plugins folder so this data is available across all themes
-/* function university_post_types()
-{
-    // ('name of custom post type', 'array')
-    register_post_type('event', array(
-        'public' => true,
-        'labels' => array(
-            'name' => 'Events',
-            'add_new_item' => "Add New Event",
-            'edit_item' => "Edit Event",
-            'all_items' => 'All Events',
-            'singular_name' => 'Event'
-        ),
-        'menu_icon' => 'dashicons-calendar'
-    ));
-}
-
-// create new post type
-add_action('init', 'university_post_types');  // ('hook', 'function name') */
-
 
 function university_adjust_queries($query)
 {
@@ -62,18 +30,17 @@ function university_adjust_queries($query)
 
     if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
         $today = date('Ymd');
-
         $query->set('meta_key', 'event_date');
         $query->set('orderby', 'meta_value_num');
         $query->set('order', 'ASC');
         $query->set('meta_query', array(
-            array(
-              'key' => 'event_date',
-              'compare' => '>=',
-              'value' => $today,
-              'type' => 'numeric'
-            )
-          ));
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            ));
     }
 }
 
--- single-event.php
+++ single-event.php
@@ -1,63 +1,56 @@
-<!-- Events posts: WP looks for custom posts through single-##### (post type) -->
-
-
 <?php
+  
+  get_header();
 
-get_header();
-
-while (have_posts()) {
-    the_post(); ?>
-
+  while (have_posts()) {
+      the_post(); ?>
 <div class="page-banner">
   <div class="page-banner__bg-image"
-    style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>);">
-  </div>
+    style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title"><?php the_title(); ?></h1>
     <div class="page-banner__intro">
-      <p>--DON'T FORGET TO REPLACE ME LATER--</p>
+      <p>DONT FORGET TO REPLACE ME LATER</p>
     </div>
   </div>
 </div>
 
 <div class="container container--narrow page-section">
   <div class="metabox metabox--position-up metabox--with-home-link">
-    <p>
-      <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>">
-        <i class="fa fa-home" aria-hidden="true"></i> Events Home
-      </a>
-      <span class="metabox__main"><?php the_title(); ?></span></p>
+    <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i
+          class="fa fa-home" aria-hidden="true"></i> Events Home</a> <span
+        class="metabox__main"><?php the_title(); ?></span></p>
   </div>
 
-  <div class="generic-content">
-    <?php the_content(); ?>
-  </div>
+  <div class="generic-content"><?php the_content(); ?></div>
 
   <?php
-$relatedPrograms = get_field('related_programs');
 
-    if ($relatedPrograms) {
-        echo '<hr class="section-break">';
-        echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
-        echo '<ul class="link-list min-list>';
+      $relatedPrograms = get_field('related_programs');
 
-        foreach ($relatedPrograms as $program) {
-            ?>
+      if ($relatedPrograms) {
+          echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+          
+          echo '<ul class="link-list min-list">';
+          
+          foreach ($relatedPrograms as $program) { ?>
 
-  <li><a href="<?php echo get_the_permalink($program) ?>"><?php echo get_the_title($program); ?></a></li>
+  <li>
+    <a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a>
+  </li>
 
-  <?php
-        }
-        echo '</ul>';
-    } ?>
+  <?php }
+          echo '</ul>';
+      } ?>
 
 </div>
 
 
 
-
 <?php
-}
+  }
 
-get_footer();
+  get_footer();
+
 ?>
