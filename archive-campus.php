f3e039d2825f87d8d5b27f5232f68aa1a371c136
Sam <super8989@gmail.com>
2020-04-27 12:19:46 -0400

    Campuses continued

--- archive-campus.php
+++ archive-campus.php
@@ -12,18 +12,20 @@
 
     <div class="acf-map">
 
-  <?php
-    while (have_posts()) {
-      the_post();
-      $mapLocation = get_field('map_location');
-  ?>
-  
-    <div class='marker' data-lat='<?php echo $mapLocation['lat'] ?>' data-lng='<?php echo $mapLocation['lng'] ?>'></div>
+      <?php
+        while (have_posts()) {
+          the_post();
+          $mapLocation = get_field('map_location');
+      ?>
 
-  <?php
-    }
-    echo paginate_links();
-  ?>
+          <div class='marker' data-lat='<?php echo $mapLocation['lat'] ?>' data-lng='<?php echo $mapLocation['lng'] ?>'>
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <?php echo $mapLocation['address'] ?>
+          </div>
+
+        <?php 
+        } 
+        ?>
 
     </div>
 
