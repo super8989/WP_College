1e5af45fbe00b7fb31aa2b0c9e8b57be09e392a7
Sam <super8989@gmail.com>
2020-04-27 15:15:12 -0400

    Managing time in JavaScript

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -6,8 +6,10 @@ class Search {
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
+    this.searchField = $("#search-term");
     this.events();
     this.isOverlayOpen = false;
+    this.typingTimer;
   }
 
 
@@ -17,11 +19,17 @@ class Search {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
     $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.on("keydown", this.typingLogic.bind(this));
   }
   
 
 
   // 3. Methods (function, action...)
+  typingLogic() {
+    clearTimeout(this.typingTimer);
+    this.typingTimer = setTimeout(function() {console.log('timeout test');},2000);
+  }
+
   keyPressDispatcher(e) {
     if (e.keyCode == 83 && !this.isOverlayOpen) {
       this.openOverlay();
