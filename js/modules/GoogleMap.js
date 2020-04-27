6270b2b1add7b711f2529507ebba01379e2d93ad
Sam <super8989@gmail.com>
2020-04-26 21:53:13 -0400

    Campus map on frontend

--- js/modules/GoogleMap.js
+++ js/modules/GoogleMap.js
@@ -1,102 +1,93 @@
 import $ from 'jquery';
 
 class GMap {
-  constructor() {
-    var self = this;
-    $('.acf-map').each(function(){
-      self.new_map( $(this) );
-    });
+	constructor() {
+		var self = this;
+		$('.acf-map').each(function () {
+			self.new_map($(this));
+		});
+	} // end constructor
 
-  } // end constructor
+	new_map($el) {
+		// var
+		var $markers = $el.find('.marker');
 
-  new_map( $el ) {
+		// vars
+		var args = {
+			zoom: 16,
+			center: new google.maps.LatLng(0, 0),
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+		};
 
-    // var
-    var $markers = $el.find('.marker');
+		// create map
+		var map = new google.maps.Map($el[0], args);
 
-    // vars
-    var args = {
-      zoom    : 16,
-      center    : new google.maps.LatLng(0, 0),
-      mapTypeId : google.maps.MapTypeId.ROADMAP
-    };
+		// add a markers reference
+		map.markers = [];
 
-    // create map
-    var map = new google.maps.Map( $el[0], args);
+		var that = this;
 
-    // add a markers reference
-    map.markers = [];
+		// add markers
+		$markers.each(function () {
+			that.add_marker($(this), map);
+		});
 
-    var that = this;
+		// center map
+		this.center_map(map);
+	} // end new_map
 
-    // add markers
-    $markers.each(function(){
-      that.add_marker( $(this), map );
-    });
+	add_marker($marker, map) {
+		// var
+		var latlng = new google.maps.LatLng(
+			$marker.attr('data-lat'),
+			$marker.attr('data-lng')
+		);
 
-    // center map
-    this.center_map( map );
-  
-    } // end new_map
+		var marker = new google.maps.Marker({
+			position: latlng,
+			map: map,
+		});
 
-    add_marker( $marker, map ) {
+		map.markers.push(marker);
 
-    // var
-    var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
+		// if marker contains HTML, add it to an infoWindow
+		if ($marker.html()) {
+			// create info window
+			var infowindow = new google.maps.InfoWindow({
+				content: $marker.html(),
+			});
 
-    var marker = new google.maps.Marker({
-      position  : latlng,
-      map     : map
-    });
+			// show info window when marker is clicked
+			google.maps.event.addListener(marker, 'click', function () {
+				infowindow.open(map, marker);
+			});
+		}
+	} // end add_marker
 
-    map.markers.push( marker );
+	center_map(map) {
+		// vars
+		var bounds = new google.maps.LatLngBounds();
 
-    // if marker contains HTML, add it to an infoWindow
-    if( $marker.html() )
-    {
-      // create info window
-      var infowindow = new google.maps.InfoWindow({
-        content   : $marker.html()
-      });
+		// loop through all markers and create bounds
+		$.each(map.markers, function (i, marker) {
+			var latlng = new google.maps.LatLng(
+				marker.position.lat(),
+				marker.position.lng()
+			);
 
-      // show info window when marker is clicked
-      google.maps.event.addListener(marker, 'click', function() {
+			bounds.extend(latlng);
+		});
 
-        infowindow.open( map, marker );
-
-      });
-    }
-
-    } // end add_marker
-
-    center_map( map ) {
-
-    // vars
-    var bounds = new google.maps.LatLngBounds();
-
-    // loop through all markers and create bounds
-    $.each( map.markers, function( i, marker ){
-
-      var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
-
-      bounds.extend( latlng );
-
-    });
-
-    // only 1 marker?
-    if( map.markers.length == 1 )
-    {
-      // set center of map
-        map.setCenter( bounds.getCenter() );
-        map.setZoom( 16 );
-    }
-    else
-    {
-      // fit to bounds
-      map.fitBounds( bounds );
-    }
-
-    } // end center_map
+		// only 1 marker?
+		if (map.markers.length == 1) {
+			// set center of map
+			map.setCenter(bounds.getCenter());
+			map.setZoom(16);
+		} else {
+			// fit to bounds
+			map.fitBounds(bounds);
+		}
+	} // end center_map
 }
 
 export default GMap;
