fee103e07796e6d233c96435d8fa6de424dfce0c
Sam <super8989@gmail.com>
2020-04-27 14:54:39 -0400

    Keyboard events in JavaScript

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -7,6 +7,7 @@ class Search {
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
     this.events();
+    this.isOverlayOpen = false;
   }
 
 
@@ -15,17 +16,34 @@ class Search {
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
+    $(document).on("keydown", this.keyPressDispatcher.bind(this));
   }
   
 
 
   // 3. Methods (function, action...)
+  keyPressDispatcher(e) {
+    if (e.keyCode == 83 && !this.isOverlayOpen) {
+      this.openOverlay();
+    }
+
+    if (e.keyCode == 27 && this.isOverlayOpen) {
+      this.closeOverlay();
+    }
+  }
+
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
+    $("body").addClass("body-no-scroll");
+    console.log('our open method just ran');
+    this.isOverlayOpen = true;
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass('search-overlay--active');
+    $("body").removeClass("body-no-scroll");
+    console.log('our close method just ran');
+    this.isOverlayOpen = false;
   }
 }
 
