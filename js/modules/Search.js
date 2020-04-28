b3408145beb14e8d22889b0a08786e7777507c36
Sam <super8989@gmail.com>
2020-04-27 20:46:52 -0400

    Quick miscellaneous edits

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 class Search {
   // 1. Describe an create/initiate our object
   constructor() {
+    this.addSearchHTML();
     this.resultsDiv = $("#search-overlay__results");
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
@@ -37,7 +38,7 @@ class Search {
 					this.resultsDiv.html('<div class="spinner-loader"></div>');
 					this.isSpinnerVisible = true;
 				}
-				this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+				this.typingTimer = setTimeout(this.getResults.bind(this), 750);
       } else {
         this.resultsDiv.html('');
         this.isSpinnerVisible = false;
@@ -71,6 +72,8 @@ class Search {
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
+    this.searchField.val('');
+    setTimeout(() => this.searchField.focus(), 301);
     console.log('our open method just ran');
     this.isOverlayOpen = true;
   }
@@ -80,6 +83,24 @@ class Search {
     $("body").removeClass("body-no-scroll");
     console.log('our close method just ran');
     this.isOverlayOpen = false;
+  }
+
+  addSearchHTML() {
+    $('body').append(`
+      <div class="search-overlay">
+        <div class="search-overlay__top">
+          <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+          </div>
+        </div>
+
+        <div class="container">
+          <div id="search-overlay__results"></div>
+        </div>
+      </div>
+    `);
   }
 }
 
