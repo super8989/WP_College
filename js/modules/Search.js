4bc1d5bb080337f96cea089bab438c0d8c07e118
Sam <super8989@gmail.com>
2020-04-27 21:06:28 -0400

    Async api request

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -48,10 +48,11 @@ class Search {
   }
 
   getResults() {
-    $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
-      $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val(), pages => {
-        var combinedResults = posts.concat(pages);
-
+    $.when(
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()), 
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
+      ).then((posts, pages) => {
+      var combinedResults = posts[0].concat(pages[0]);
         this.resultsDiv.html(`
           <h2 class="search-overlay__section-title">General Information</h2>
           ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search</p>'}
@@ -59,8 +60,8 @@ class Search {
           ${combinedResults.length ? '</ul>' : ''}
         `);
 					this.isSpinnerVisible = false;
-				}
-			);
+    }, () => {
+      this.resultsDiv.html('<p>Unexpected error. Please try again.</p>')
     });
   }
 
