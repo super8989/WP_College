eacc099e4ba09ac547f8858abe0f7450970ed36b
Sam <super8989@gmail.com>
2020-04-28 16:05:15 -0400

    Search logic that is aware of relationships

--- inc/search-route.php
+++ inc/search-route.php
@@ -59,7 +59,7 @@
 
       if (get_post_type() == 'event') {
         $eventDate = new DateTime(get_field('event_date'));
-        
+
         $description = null;
 
         if (has_excerpt()) {
@@ -78,6 +78,31 @@
         ));
       }
     }
+
+
+    $programRelationshipQuery = new WP_Query(array(
+      'post_type' => 'professor',
+      'meta_query' => array(
+        array(
+          'key' => 'related_programs',
+          'compare' => 'LIKE',
+          'value' => '"84"'
+      ))
+    ));
+
+    while ($programRelationshipQuery->have_posts()) {
+      $programRelationshipQuery->the_post();
+
+      if (get_post_type() == 'professor') {
+        array_push($results['professors'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink(),
+          'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+        ));
+      }
+    }
+
+    $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
 
     return $results;
   }
