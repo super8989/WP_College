8dc0263e6cf6aba1f390003f0175afed3f5a3b59
Sam <super8989@gmail.com>
2020-04-29 12:45:05 -0400

    Open registration

--- functions.php
+++ functions.php
@@ -108,3 +108,30 @@ function universityMapKey($api) {
 
 add_filter('acf/fields/google_map/api', 'universityMapKey');
 
+
+// Redirect subscriber accounts out of admin and onto homepage
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function redirectSubsToFrontend() {
+    $ourCurrentUser = wp_get_current_user();
+
+    if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->roles[0] == 'subscriber') {
+        wp_redirect(site_url('/'));
+        exit;
+    }
+}
+
+
+// Remove admin bar from the top of homepage 
+add_action('wp-loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar()
+{
+    $ourCurrentUser = wp_get_current_user();
+
+    if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->roles[0] == 'subscriber') {
+        show_admin_bar(false);
+    }
+}
+
+
