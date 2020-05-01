e87ba121294e305e98d8973a982319d9b02bf3de
Sam <super8989@gmail.com>
2020-05-01 14:16:01 -0400

    Query vars

--- page.php
+++ page.php
@@ -48,7 +48,23 @@
     
 
     <div class="generic-content">
-      <?php the_content(); ?>
+      <?php the_content(); 
+
+        $skyColorValue = sanitize_text_field(get_query_var('skyColor'));
+        $grassColorValue = sanitize_text_field(get_query_var('grassColor'));
+
+        if ($skyColorValue == 'blue' and $grassColorValue == 'green') {
+          echo '<p>Sky is blue today and grass is green. Life is good.</p>';
+        }
+      
+      ?>
+
+      <form method="get">
+        <input name="skyColor" placeholder="Sky color">
+        <input name="grassColor" placeholder="Grass color">
+        <button>Submit</button>
+      </form>
+
     </div>
 
   </div>
